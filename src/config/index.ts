6d43a20256fb6a92502ea7bcb501b505a17b66ca
yolk3202 <yolk3202@163.com>
2023-11-26 00:56:43 +0800

    配置

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,21 +1,21 @@
-import {modeMap, envMap} from './configMap.js'
+import { envMap } from './configMap.js'
 import dev from './dev.js'
 import prod from './prod.js'
 import { ConfigType } from './types'
 
 let configObj: ConfigType = {
   env: '',
-  api: '',
+  api: {},
   baseUrl: '',
 };
 
 const config: ConfigType = (() => {
   const env = process.env.NODE_ENV;
   console.log('env=====>', env)
-  if(env === envMap.DEV){
+  if (env === envMap.DEV) {
     configObj = { ...configObj, ...dev };
   }
-  if(env === envMap.PROD){
+  if (env === envMap.PROD) {
     configObj = { ...configObj, ...prod };
   }
   return configObj;
