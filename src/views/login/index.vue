55eb2ae509e6d643f4ee4694624844abe3f2ab04
yolk3202 <yolk3202@163.com>
2023-12-13 22:05:40 +0800

    移除注册

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -108,20 +108,17 @@
           >{{ $t("login.login") }}
         </el-button>
 
-        <div class="register-line">
+        <!--
+          <div class="register-line">
           <el-button
-          type="primary"
-          class="register-btn"
-          @click.prevent="handleRegister"
-          >{{ $t("login.register") }}
-        </el-button>
+            type="primary"
+            class="register-btn"
+            @click.prevent="handleRegister"
+            >{{ $t("login.register") }}
+          </el-button>
         </div>
-
-        <!-- 账号密码提示 -->
-        <!-- <div class="mt-10 text-sm">
-          <span>{{ $t("login.username") }}: admin</span>
-          <span class="ml-4"> {{ $t("login.password") }}: 123456</span>
-        </div> -->
+        -->
+        
       </el-form>
     </el-card>
 
