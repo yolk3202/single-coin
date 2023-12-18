07539dfeb1569190a5658980c08db7d67cc2572d
yolk3202 <yolk3202@163.com>
2023-12-19 00:17:43 +0800

    对敲机器人-改名

--- src/views/robotSystem/robotControl/model.js
+++ src/views/robotSystem/robotControl/model.js
@@ -1,13 +1,13 @@
 export const robotList = [
   {name: '近盘机器人', value: 'near_plate'},
   {name: '远盘机器人', value: 'far_plate'},
-  {name: '划线机器人', value: 'draw_line'},
+  {name: '对敲机器人', value: 'draw_line'},
   {name: '大单机器人', value: 'big_order'},
 ]
 export const robots = {
   'near_plate': {name: '近盘机器人', value: 'near_plate', status:false, loading:true },
   'far_plate': {name: '远盘机器人', value: 'far_plate', status:false, loading:true },
-  'draw_line': {name: '画线机器人', value: 'draw_line', status:false, loading:true },
+  'draw_line': {name: '对敲机器人', value: 'draw_line', status:false, loading:true },
   'big_order': {name: '大单机器人', value: 'big_order', status:false, loading:true },
 }
 export const robotTypes = ['near_plate', 'far_plate', 'draw_line', 'big_order']
